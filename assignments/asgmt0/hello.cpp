ae3720dced8d285536b31bf526f2eb1e68907960
mllavez <marcos@llavez.com>
2024-06-20 20:52:30 -0700

    feat: compleated asgmt1 & cleanup

---
+++ assignments/asgmt0/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Marcos Alexander Chavez" << endl;
+    cout << "marcos@llavez.com" << endl;
+    cout << "CS11 Summer 2024" << endl;
+    return 0;
+}
+
