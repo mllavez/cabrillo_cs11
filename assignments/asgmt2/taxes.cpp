7330f824cfc3c62f9de7d5c9f81bc4fd713ad11a
mllavez <marcos@llavez.com>
2024-06-23 18:58:27 -0700

    feat: updated asgmt2

--- assignments/asgmt2/taxes.cpp
+++ assignments/asgmt2/taxes.cpp
@@ -4,34 +4,57 @@
 using namespace std;
 
 int main(){
-    double income, tax = 0.0;
+    double income, tax, remain = 0.0;
 
     cout << "What were your earnings (enter dollars & cents as a decimal without a dollar sign. i.e. 70000.80)";
     cin >> income;
+    remain = 0
 
-    if (income > 500000) {
-        tax += (income - 500000) * 0.06;
-        income = 500000;
+    while( remain > 0 ) {
+        tax = .01 * income;
+        if(income <= 50000){
+            remain = 0;
+        }
+        remain = income - 50000;
+        double bracket2_diff = 75000 - 50000;
+
+        if(remain <= bracket2_diff){
+            tax += .02 * remain;
+            remain = 0;
+        }
+        remain -= bracket2_diff;
+        tax += .02 * bracket2_diff;
+
+        double bracket3_diff = 100000 - 75000;
+
+        if(remain <= bracket3_diff){
+            tax += .03 * remain;
+            remain = 0;
+        }
+        remain -= bracket3_diff;
+        tax += .03 * bracket3_diff;
+
+        double bracket4_diff = 250000 - 100000;
+        if(remain <= bracket4_diff){
+            tax += .04 * remain;
+            remain = 0;
+        }
+        remain -= bracket4_diff;
+        tax += .04 * bracket4_diff;
+
+        double bracket5_diff = 500000 - 250000;
+        if(remain <= bracket5_diff){
+            tax += .05 * remain;
+            remain = 0;
+        }
+        remain -= bracket5_diff;
+        tax += .05 * bracket5_diff;
+
+
+        remain = 0;
     }
-    if (income > 250000) {
-        tax += (income - 250000) * 0.05;
-        income = 250000;
-    }
-    if (income > 100000) {
-        tax += (income - 100000) * 0.04;
-        income = 100000;
-    }
-    if (income > 75000) {
-        tax += (income - 75000) * 0.03;
-        income = 75000;
-    }
-    if (income > 50000) {
-        tax += (income - 50000) * 0.02;
-        income = 50000;
-    }
-    if (income > 0) {
-        tax += income * 0.01;
-    }
+
+
     cout << "You would owe: $ " << fixed << setprecision(2) << tax;
     return 0;
 }
