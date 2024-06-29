b17d663780773fe217344f4210c92cc4fd0b7e61
mllavez <marcos.chavez@freeagentsoftware.com>
2024-06-29 13:40:30 -0700

    fix: updated asgmt2 taxes.cpp

--- assignments/asgmt2/taxes.cpp
+++ assignments/asgmt2/taxes.cpp
@@ -5,57 +5,76 @@ using namespace std;
 
 int main(){
     double income, tax, remain = 0.0;
+    const double
+    RATE_1 = .01,
+    RATE_2 = .02,
+    RATE_3 = .03,
+    RATE_4 = .04,
+    RATE_5 = .05,
+    RATE_6 = .06,
+    TAX_STEP_1 = 50000,
+    TAX_STEP_2 = 75000,
+    BRACKET_1_DIFF = TAX_STEP_2 - 1 - TAX_STEP_1,
+    TAX_STEP_3 = 100000,
+    BRACKET_2_DIFF = TAX_STEP_3 - 1 - TAX_STEP_2,
+    TAX_STEP_4 = 250000,
+    BRACKET_3_DIFF = TAX_STEP_4 - 1 - TAX_STEP_3,
+    TAX_STEP_5 = 500000,
+    BRACKET_4_DIFF = TAX_STEP_5 - 1 - TAX_STEP_4;
 
     cout << "What's your gross income? (Enter a income without a dollar sign. i.e. 700000.80)\n";
     cin >> income;
     remain = income;
 
-    while( remain > 0 ) {
-        tax = .01 * income;
-        if(income <= 50000){
-            remain = 0;
-        }
-        remain = income - 50000;
-        double bracket2_diff = 75000 - 50000;
+    if(income <= TAX_STEP_1){
+        tax = RATE_1 * remain;
+        cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
+        return 0;
+    }
+    tax = RATE_1 * TAX_STEP_1;
 
-        if(remain <= bracket2_diff){
-            tax += .02 * remain;
-            remain = 0;
-        }
-        remain -= bracket2_diff;
-        tax += .02 * bracket2_diff;
+    remain = income - TAX_STEP_1;
 
-        double bracket3_diff = 100000 - 75000;
+    if(remain <= BRACKET_1_DIFF){
+        tax += RATE_2 * remain;
+        cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
+        return 0;
+    }
 
-        if(remain <= bracket3_diff){
-            tax += .03 * remain;
-            remain = 0;
-        }
-        remain -= bracket3_diff;
-        tax += .03 * bracket3_diff;
+    remain -= BRACKET_1_DIFF;
+    tax += RATE_2 * BRACKET_1_DIFF;
 
-        double bracket4_diff = 250000 - 100000;
-        if(remain <= bracket4_diff){
-            tax += .04 * remain;
-            remain = 0;
-        }
-        remain -= bracket4_diff;
-        tax += .04 * bracket4_diff;
+    if(remain <= BRACKET_2_DIFF){
+        tax += RATE_3 * remain;
+        cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
+        return 0;
+    }
 
-        double bracket5_diff = 500000 - 250000;
-        if(remain <= bracket5_diff){
-            tax += .05 * remain;
-            remain = 0;
-        }
-        remain -= bracket5_diff;
-        tax += .05 * bracket5_diff;
+    remain -= BRACKET_2_DIFF;
+    tax += RATE_3 * BRACKET_2_DIFF;
 
-        if(remain > 0){
-            tax += .06 * remain;
-            remain = 0;
-        }
+    if(remain <= BRACKET_3_DIFF){
+        tax += RATE_4 * remain;
+        cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
+        return 0;
 
-        remain = 0;
+    }
+    remain -= BRACKET_3_DIFF;
+    tax += RATE_4 * BRACKET_3_DIFF;
+
+    if(remain <= BRACKET_4_DIFF){
+        tax += RATE_5 * remain;
+        cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
+        return 0;
+    }
+    remain -= TAX_STEP_4;
+    tax += RATE_5 * BRACKET_4_DIFF;
+
+    if(remain > 0){
+        tax += RATE_7 * remain;
+        cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
+        return 0;
+
     }
 
     cout << "Your total tax liability, circa 1913, would be\n$" << fixed << setprecision(2) << tax << "\n";
