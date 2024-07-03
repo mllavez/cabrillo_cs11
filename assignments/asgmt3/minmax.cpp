8e6cef8ef119fa33b7f1b87e55e1febe3217edd4
Marcos Alexander Chávez <marcos@llavez.com>
2024-07-03 16:13:53 +0000

    feat: continuing asgmt3 minmax.cpp

--- assignments/asgmt3/minmax.cpp
+++ assignments/asgmt3/minmax.cpp
@@ -1,9 +1,38 @@
-// Marcos A. Chavez
-// marcos@llavez.com
+/** Marcos A. Chavez
+* marcos@llavez.com
+*
+* Input multiple doubles
+* returns minimum, maximum, and average
+*/
 
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    cout << "
+    double sum = 0.0, temp = sum, prev = sum, min = sum, max = sum;
+    int count = 0;
+
+    cout << "Enter doubles, EOF to quit: \n";
+
+    while(cin >> temp){
+        prev = temp;
+        if(temp < prev){
+            min = temp;
+        }
+        if(temp > prev){
+            max = temp;
+        }
+        sum += temp;
+        count++;
+    }
+    if (!count){
+        cout << "no input\n";
+        return 0;
+    }
+    cout << "The min is " << min << ".\n";
+    cout << "The max is " << max << ".\n";
+    cout << "The average is" << sum/count << ".\n";
+    return 0;
+}
+
